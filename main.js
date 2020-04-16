68517bca47f18faac2232dfc3dc73a1a45daa40b
Alfredo Rodriguez <rcalfredoe@gmail.com>
2020-04-15 21:12:38 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function Show(id) {
+    let element = document.getElementById(id);
+    element.style.display = "initial";
+}
+
+$(document).ready(function(){
+    $("p").click(function(){
+      
+    });
+});
