e545c56305c2eba86a8ebef6d07151c233f14431
Alfredo Rodriguez <rcalfredoe@gmail.com>
2020-04-15 23:12:11 -0700

    .

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 function Show(id) {
     let element = document.getElementById(id);
-    element.style.display = "initial";
+    element.style.display = "block";
+    element.classList.add("container");
+    element.classList.add("text-center");
+    
+
+    let divVideo = document.getElementById('idDivVideo')
+
+    let divVideoDisplay = divVideo.style.display;
+
+    if (divVideoDisplay !== "none") {
+        
+        divVideo.style.display = "none";
+    }
+
 }
 
 $(document).ready(function(){
